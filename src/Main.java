fcb0830917774f4890419d7d62532042f5a02863
idkpizzaman <idkekdms@gmail.com>
2024-05-03 14:59:16 +0900

    번호를 매겨서 기사 저장하기!

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,40 @@ public class Main {
 		System.out.println("== 프로그램 시작 ==");
 		
 		Scanner sc = new Scanner(System.in);
+		int listnumber = 0;
 		
 		while(true) {			
 			System.out.println("명령어를 입력하세요: ");
 			String cmd = sc.nextLine();
-		
-			if (cmd.equals("종료")) {
+			
+			if (cmd.equals("article write")) {
+				System.out.println("제목: ");
+				String title = sc.nextLine();
+				System.out.println("내용: ");
+				String content = sc.nextLine();
+				
+				listnumber += 1;
+				System.out.println(listnumber + "번 글이 생성되었습니다.");
+			}
+			
+			if (cmd.equals("article list")) {
+				System.out.println("게시글이 없습니다.");
+			}
+			
+			if (cmd.equals("exit")) {
 				System.out.println("프로그램을 종료합니다.");
-				System.out.println("== 프로그램 끝 ==");
 				break;
 			}
+			
 		}
+		
 		sc.close();
+		System.out.println("== 프로그램 끝 ==");
+	}
+}
+
+class ArticleListUpload{
+	void listupload() {
+		
 	}
 }
